b13c156a43ef116e0e8c37e4913d552e6914b830
Bernd Zeimetz <bernd@bzed.de>
2016-02-04 16:16:06 +0100

    Fix crt shipment.

--- manifests/deploy/crt.pp
+++ manifests/deploy/crt.pp
@@ -1,3 +1,16 @@
+# = Define: letsencrypt::crt
+#
+# Used as exported ressource to ship a signed CRT.
+#
+# == Parameters:
+#
+# [*crt_file*]
+#   File which contains the crt.
+#
+# [*domain*]
+#   Certificate commonname / domainname.
+#
+
 define letsencrypt::deploy::crt(
     $crt_file,
     $domain = $name
@@ -7,10 +20,10 @@ define letsencrypt::deploy::crt(
 
     $handler_requests_dir = $::letsencrypt::params::handler_requests_dir
     $base_dir = "${handler_requests_dir}/${domain}"
-    $crt_file = "${base_dir}/${domain}.crt"
+    $crt      = "${base_dir}/${domain}.crt"
 
 
-    file { $crt_file :
+    file { $crt :
         ensure  => file,
         owner   => root,
         group   => letsencrypt,
