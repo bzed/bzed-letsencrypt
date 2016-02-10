3b1be61e27e4b45e3674a6536251dc15c2982de4
Dennis Hoppe <github@debian-solutions.de>
2016-02-10 15:02:13 +0100

    Fix several rubocop issues

--- lib/facter/letsencrypyt_csr.rb
+++ lib/facter/letsencrypyt_csr.rb
@@ -1,24 +1,19 @@
 
 require 'facter'
 
-csr_domains = Dir['/etc/letsencrypt/csr/*.csr'].map{|a| a.gsub(/\.csr$/, '').gsub(/^.*\//, '') }
+csr_domains = Dir['/etc/letsencrypt/csr/*.csr'].map { |a| a.gsub(/\.csr$/, '').gsub(%r{^.*/, ''}) }
 
 Facter.add(:letsencrypt_csrs) do
+  setcode do
+    csr_domains.join(',') if csr_domains
+  end
+end
+
+csr_domains.each do |csr_domain|
+  Facter.add('letsencrypt_csr_' + csr_domain) do
     setcode do
-        if csr_domains
-            csr_domains.join(',')
-        else
-            nil
-        end
+      csr = File.read("/etc/letsencrypt/csr/#{csr_domain}.csr")
+      csr
     end
+  end
 end
-
-csr_domains.each do | csr_domain |
-    Facter.add("letsencrypt_csr_" + csr_domain) do
-        setcode do
-            csr = File.read("/etc/letsencrypt/csr/#{csr_domain}.csr")
-            csr
-        end
-    end
-end
-
