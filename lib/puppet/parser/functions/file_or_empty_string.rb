04e1cf6e8dd7d5f657ce76ea4e236637567e9461
Bernd Zeimetz <bernd@bzed.de>
2016-08-23 11:26:44 +0200

    Fix some rubocpo issues

--- lib/puppet/parser/functions/file_or_empty_string.rb
+++ lib/puppet/parser/functions/file_or_empty_string.rb
@@ -2,14 +2,15 @@
 # if the file does not exist. based on file.rb from puppet.
 
 Puppet::Parser::Functions.newfunction(
-  :file_or_empty_string, :type => :rvalue,
-
-                         :doc => "Return the contents of a file.  Multiple files
-    can be passed, and the first file that exists will be read in.") do |vals|
+  :file_or_empty_string,
+  type: :rvalue,
+  doc:  "Return the contents of a file.  Multiple files
+         can be passed, and the first file that exists will be read in."
+) do |vals|
   ret = nil
   vals.each do |file|
     unless Puppet::Util.absolute_path?(file)
-      fail Puppet::ParseError, 'Files must be fully qualified'
+      raise(Puppet::ParseError, 'Files must be fully qualified')
     end
     if FileTest.exists?(file)
       ret = File.read(file)
