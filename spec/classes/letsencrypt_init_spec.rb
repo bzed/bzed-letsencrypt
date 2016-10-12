fddb1fda44370c850078f8308dd8f142e4053c74
Bernd Zeimetz <bernd@bzed.de>
2016-10-12 18:54:54 +0200

    some basic stuff

--- spec/classes/letsencrypt_init_spec.rb
+++ spec/classes/letsencrypt_init_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 describe 'letsencrypt' do
-    let :facts do
-        {
-            osfamily: 'Ubuntu',
-            fqdn: 'spec-test.example.com',
-            puppetmaster: 'spec-test.example.com'
-        }
-    end
+  let :facts do
+    {
+      osfamily: 'Debian',
+      fqdn: 'spec-test.example.com',
+      puppetmaster: 'spec-test.example.com'
+    }
+  end
+  it { should contain_file('/etc/letsencrypt').with_ensure('directory') }
+  it { should contain_file('/etc/letsencrypt/csr').with_ensure('directory') }
+  it { should contain_file('/etc/letsencrypt/private').with_ensure('directory') }
+  it { should contain_file('/etc/letsencrypt/certs').with_ensure('directory') }
+
 end
