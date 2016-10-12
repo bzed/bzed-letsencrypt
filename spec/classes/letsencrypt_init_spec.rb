ce9b2ad936826e335b2a170f70284a833d51079f
Bernd Zeimetz <bernd@bzed.de>
2016-10-12 12:38:22 +0200

    add some initial spec test code

--- spec/classes/letsencrypt_init_spec.rb
+++ spec/classes/letsencrypt_init_spec.rb
@@ -1,3 +1,10 @@
 require 'spec_helper'
 describe 'letsencrypt' do
+    let :facts do
+        {
+            osfamily: 'Ubuntu',
+            fqdn: 'spec-test.example.com',
+            puppetmaster: 'spec-test.example.com'
+        }
+    end
 end
