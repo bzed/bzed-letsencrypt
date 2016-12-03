a2ef19930835a5c5a029ff91849a71a0d1ee926b
SiliconAlchemy <info@siliconalchemy.net>
2016-12-03 19:13:03 +0000

    Change init params to inherited, fixes #23

--- manifests/params.pp
+++ manifests/params.pp
@@ -30,4 +30,18 @@ class letsencrypt::params {
 
     $letsencrypt_chain_request = "${handler_base_dir}/letsencrypt_get_certificate_chain.sh"
     $letsencrypt_ocsp_request = "${handler_base_dir}/letsencrypt_get_certificate_ocsp.sh"
+
+    if defined('$puppetmaster') {
+        $letsencrypt_host = $::puppetmaster
+    } elsif defined('$servername') {
+        $letsencrypt_host = $::servername
+    }
+
+    $letsencrypt_sh_git_url = 'https://github.com/lukas2511/dehydrated.git'
+    $dehydrated_git_url = $letsencrypt_sh_git_url
+    $challengetype = 'dns-01'
+    $letsencrypt_ca = 'https://acme-v01.api.letsencrypt.org/directory'
+    $dh_param_size = 2048
+    $manage_packages = true
+
 }
