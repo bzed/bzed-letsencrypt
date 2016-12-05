82aa8a5a8f40c4699e85459b7dc9927db2df1b4a
SiliconAlchemy <info@siliconalchemy.net>
2016-12-05 12:13:53 +0000

    Alter facter libs to use dynamic ppath files, #21

--- lib/facter/letsencrypyt_crt.rb
+++ lib/facter/letsencrypyt_crt.rb
@@ -1,7 +1,12 @@
 
 require 'facter'
 
-crt_domains = Dir['/opt/letsencrypt/requests/*/*.crt'].map { |a| a.gsub(%r{\.crt$}, '').gsub(%r{^.*/}, '') }
+begin
+  basedir = File.read("/etc/letsencrypt.handler_basedir").chomp
+rescue
+  basedir = "/opt/letsencrypt"
+end
+crt_domains = Dir["#{basedir}/requests/*/*.crt"].map { |a| a.gsub(%r{\.crt$}, '').gsub(%r{^.*/}, '') }
 
 Facter.add(:letsencrypt_crts) do
   setcode do
