9d5c4dd7f4bb18be7b06fb62be9afa7753d50b0c
Bernd Zeimetz <bernd@bzed.de>
2018-04-20 11:14:00 +0200

    Handle CSRs without SAN extention in check_altnames.

--- files/letsencrypt_check_altnames.sh
+++ files/letsencrypt_check_altnames.sh
@@ -6,7 +6,13 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 CSR=${1}
 CRT=${2}
 
-declare -a CSR_DNS=($(openssl req -text -noout -in ${CSR} | awk '/DNS/ {print}' | sed s/,//g))
+if openssl req -text -noout -in ${CSR} | grep -q DNS; then
+    declare -a CSR_DNS=($(openssl req -text -noout -in ${CSR} | awk '/DNS/ {print}' | sed s/,//g))
+else
+    # we have an old CSR without DNS/SAN features.
+    declare -a CSR_DNS=($(openssl req -in ${CSR}  -text | awk '/Subject: CN=/' | sed 's,.*CN=,DNS:,'))
+fi
+
 declare -a CRT_DNS=($(openssl x509 -text -noout -in ${CRT} -certopt no_subject,no_header,no_version,no_serial,no_signame,no_validity,no_subject,no_issuer,no_pubkey,no_sigdump,no_aux | awk '/DNS/ {print}' | sed s/,//g))
 declare -a DIFF=()
 
